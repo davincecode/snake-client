06ac4f8a75c0c32058f87811b08a140e19bf66f0
davincecode <hyperscript.ca@gmail.com>
2022-02-16 23:34:34 +0000

    play connection established

--- play.js
+++ play.js
@@ -4,15 +4,15 @@ const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
     host: "165.227.47.243", // IP address here,
-    port: "50541", // PORT number here,
+    port: "50541", // PORT, number here,
+  });
+
+  conn.on("connect", () => {
+    console.log("CONIKTED"); // code that does something when the connection is first established
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log("Connected"); // code that does something when the connection is first established
-  });
 
   return conn;
 };
